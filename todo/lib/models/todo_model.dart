68a0464003ae06715eddf4db7adf85caab51a760
suman2023 <sumanmitra07@outlook.com>
2022-04-20 00:53:58 +0530

    initial models

---
+++ todo/lib/models/todo_model.dart
@@ -0,0 +1,12 @@
+class Todo {
+  int id;
+  String title, description;
+  DateTime dateTime;
+
+  Todo({
+    required this.id,
+    required this.title,
+    required this.description,
+    required this.dateTime,
+  });
+}
